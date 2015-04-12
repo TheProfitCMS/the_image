c80c8bb2815a0db16891f887a3b799d78ad62fae
Ilya N. Zykin <zykin-ilya@ya.ru>
2015-04-12 19:43:58 +0300

    looks_fine

--- lib/the_image.rb
+++ lib/the_image.rb
@@ -2,28 +2,40 @@ require_relative 'the_image/version'
 require 'mini_magick'
 
 module TheImage
-  # Helpers
+  def chmod_mask
+    0644
+  end
+
+  # HELPERS
+  def image_open path
+    ::MiniMagick::Image.open path
+  end
+
   def path_to_file file_path
     file_path.split('/')[0...-1].join('/')
-  end
-
-  def destroy_file file_path
-    FileUtils.rm(file_path , force: true)
-  end
-
-  def destroy_dir dir_path
-    FileUtils.rm_rf dir_path
   end
 
   def create_path_for_file file_path
     FileUtils.mkdir_p path_to_file(file_path)
   end
 
+  def create_path_for_dir path
+    FileUtils.mkdir_p path
+  end
+
+  def destroy_file file_path
+    FileUtils.rm(file_path , force: true)
+  end
+
   def destroy_dir_of_file file_path
     FileUtils.rm_rf path_to_file(file_path)
   end
 
-  # Main methods
+  def destroy_dir dir_path
+    FileUtils.rm_rf dir_path
+  end
+
+  # BASE METHODS
   def landscape? image
     image[:width] > image[:height]
   end
@@ -36,29 +48,37 @@ module TheImage
     src    = opts[:src]
     dest   = opts[:dest]
     format = opts[:format]
+    image  = image_open src
 
-    image  = ::MiniMagick::Image.open src
     image.format(format.to_s.downcase) if format
-
     image = instance_exec(image, opts, &block)
+
+    create_path_for_file(dest)
     image.write dest
+
+    FileUtils.chmod(chmod_mask, dest)
   end
 
   # Image manipulate
   # Resizing can be wrong when .EXT of file is wrong
   def strict_resize image, w, h
     image.resize "#{ w }x#{ h }!"
-    image
   end
 
   def resize image, w, h
     image.resize "#{ w }x#{ h }"
-    image
+  end
+
+  def resize_w image, w
+    image.resize "#{ w }x"
+  end
+
+  def resize_h image, h
+    image.resize "x#{ h }"
   end
 
   def rotate image, angle
     image.rotate angle
-    image
   end
 
   def rotate_left image
@@ -69,26 +89,35 @@ module TheImage
     rotate image, '90'
   end
 
+  # OPTIMIZE
+
+  def strip image
+    image.strip
+  end
+
+  def auto_orient image
+    image.auto_orient
+  end
+
+  def optimize image, quality = 85, depth = 8, interlace = :plane
+    image.combine_options do |c|
+      c.quality   quality.to_s
+      c.depth     depth.to_s
+      c.interlace interlace.to_s
+    end
+  end
+
+  # USEFUL METHODS
+
   def biggest_side_not_bigger_than image, size
     if landscape?(image)
       image.resize("#{ size }x") if image[:width] > size.to_i
     else
       image.resize("x#{ size }") if image[:height] > size.to_i
     end
-
-    image
   end
 
-  def strip image
-    image.strip
-    image
-  end
-
-  def auto_orient image
-    image.auto_orient
-    image
-  end
-
+  # get rectangle form image
   def to_rect image, width, height, opts = {}
     default_opts = { valign: :center, align: :center }
     opts = default_opts.merge(opts)
@@ -125,9 +154,29 @@ module TheImage
 
     image.crop   "#{ fw }x#{ fh }+#{ x0 }+#{ y0 }"
     image.resize "#{ width }x#{ height }!"
-    image
   end
 
+  # get rectangle from image from middle or top
+  # `v_ratio_min` and `v_ratio_max` define W/H ratio
+  # when we have to get middle or top part of image
+  def smart_rect image, width, height, v_ratio_min = 0.625, v_ratio_max = 0.80
+    scale_w = 1.0/(image.width.to_f/width.to_f)
+    scale_h = 1.0/(image.height.to_f/height.to_f)
+    scale   = scale_w > scale_h ? scale_w : scale_h
+
+    ratio = scale_w/scale_h
+
+    valign = :center
+    valign = :top if ratio >= v_ratio_min && ratio <= v_ratio_max
+
+    new_w = image.width*scale
+    new_h = image.height*scale
+
+    image = to_rect image, new_w, new_h,  { valign: :center, align: :center }
+    image = to_rect image, width, height, { valign: valign,  align: :center }
+  end
+
+  # just to square
   def to_square image, size, opts = {}
     to_rect image, size, size, opts
   end
@@ -142,6 +191,5 @@ module TheImage
     h = (h.to_f * scale).to_i
 
     image.crop "#{ w }x#{ h }+#{ x0 }+#{ y0 }"
-    image
   end
 end
